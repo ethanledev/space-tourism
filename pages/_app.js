0b116ff9466e0a0cd97baad0ed181040a572428d
Hieu Le <hieuhmle@gmail.com>
2022-05-06 17:19:40 -0500

    Finish nav bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,101 @@
-import '../styles/globals.css'
+import Image from "next/image";
+import "../styles/globals.css";
+import styles from "../styles/App.module.css";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useState, useRef, useEffect } from "react";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }) => {
+  const router = useRouter();
+  const [showMenu, setShowMenu] = useState(false);
+  const mobileLinksRef = useRef();
 
-export default MyApp
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      console.log("click");
+      if (
+        mobileLinksRef.current &&
+        !mobileLinksRef.current.contains(event.target)
+      ) {
+        setShowMenu(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
+  const renderLinks = (forMobile) => (
+    <div
+      className={`${styles.links} ${forMobile ? styles.mobileLinks : null}`}
+      ref={mobileLinksRef}
+    >
+      {forMobile && (
+        <div className={styles.closeButton}>
+          <Image
+            src="/icon-close.svg"
+            width={20}
+            height={21}
+            alt="close button"
+            onClick={() => setShowMenu(false)}
+          />
+        </div>
+      )}
+      <Link href="/">
+        <a
+          className={router.asPath === "/" ? styles.active : null}
+          onClick={() => setShowMenu(false)}
+        >
+          <strong>00</strong> HOME
+        </a>
+      </Link>
+      <Link href="/destination">
+        <a
+          className={router.asPath === "/destination" ? styles.active : null}
+          onClick={() => setShowMenu(false)}
+        >
+          <strong>01</strong> DESTINATION
+        </a>
+      </Link>
+      <Link href="/crew">
+        <a
+          className={router.asPath === "/crew" ? styles.active : null}
+          onClick={() => setShowMenu(false)}
+        >
+          <strong>02</strong> CREW
+        </a>
+      </Link>
+      <Link href="/technology">
+        <a
+          className={router.asPath === "/technology" ? styles.active : null}
+          onClick={() => setShowMenu(false)}
+        >
+          <strong>03</strong> TECHNOLOGY
+        </a>
+      </Link>
+    </div>
+  );
+
+  return (
+    <div className={styles.container}>
+      <nav className={styles.nav}>
+        <hr className={styles.hr} />
+        <Image src="/logo.svg" width={48} height={48} alt="logo" />
+        <div className={styles.menuButton} onClick={() => setShowMenu(true)}>
+          <Image
+            src="/icon-hamburger.svg"
+            width={24}
+            height={21}
+            alt="menu button"
+          />
+        </div>
+        {showMenu && renderLinks(true)}
+        {renderLinks(false)}
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  );
+};
+
+export default MyApp;
