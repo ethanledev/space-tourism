2cc7997d7fa89d993a5527e8ec24f2ed857902c0
Hieu Le <hieuhmle@gmail.com>
2022-05-07 15:09:50 -0500

    Remove section tag in Destination page

--- pages/destination.js
+++ pages/destination.js
@@ -30,31 +30,28 @@ const Destination = () => {
       <h1 className="tourHeading">
         <span>01</span>PICK YOUR DESTINATION
       </h1>
-      <section className={styles.section}>
-        <div className={styles.image}>
-          <Image
-            src={destinations[index].images.webp}
-            alt={destinations[index].name}
-            width={445}
-            height={445}
-          />
-        </div>
-        <div className={styles.content}>
-          <div className={styles.tabs}>{renderTabs()}</div>
-          <article className={styles.article}>
-            <h2>{destinations[index].name}</h2>
-            <p>{destinations[index].description}</p>
-            <div className={styles.travelInfo}>
-              <div className={styles.title}>AVG. DISTANCE</div>
-              <div className={styles.title}>EST. TRAVEL TIME</div>
-              <div className={styles.detail}>
-                {destinations[index].distance}
-              </div>
-              <div className={styles.detail}>{destinations[index].travel}</div>
-            </div>
-          </article>
-        </div>
-      </section>
+
+      <div className={styles.image}>
+        <Image
+          src={destinations[index].images.webp}
+          alt={destinations[index].name}
+          width={445}
+          height={445}
+        />
+      </div>
+      <div className={styles.content}>
+        <div className={styles.tabs}>{renderTabs()}</div>
+        <article className={styles.article}>
+          <h2>{destinations[index].name}</h2>
+          <p>{destinations[index].description}</p>
+          <div className={styles.travelInfo}>
+            <div className={styles.title}>AVG. DISTANCE</div>
+            <div className={styles.title}>EST. TRAVEL TIME</div>
+            <div className={styles.detail}>{destinations[index].distance}</div>
+            <div className={styles.detail}>{destinations[index].travel}</div>
+          </div>
+        </article>
+      </div>
     </main>
   );
 };
